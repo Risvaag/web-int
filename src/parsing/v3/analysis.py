c73047e1a763bd58229e004a2805a7fa24b973dd
Anders Klever Kirkeby <anders@klever-kirkeby.com>
2018-03-30 10:34:34 +0200

    First attempt at creating a cosine similarity matrix

--- src/parsing/v3/analysis.py
+++ src/parsing/v3/analysis.py
@@ -1,8 +1,12 @@
 # pylint: disable=E1127
+# pylint: disable=E0401
 
 import pandas as pd
 import numpy as np
 import os
+import sklearn.preprocessing as pp
+
+from sklearn.metrics.pairwise import cosine_similarity
 from math import floor
 
 
@@ -20,7 +24,7 @@ def init_dataframe(raw_data_file, formatted_file):
     return df
 
 
-def user_item_sparse(df):
+def user_item_sparse(df, file):
     ''' Return a sparse user item matrix '''
     df.reset_index(inplace=True)
     df.drop("ItemRating", 1, inplace=True)
@@ -44,10 +48,20 @@ def user_item_sparse(df):
     multi_df = multi_df[~multi_df.index.duplicated(keep="first")]
 
     # Store the multiindex in a csv so we can find the user ids and article urls later
-    multi_df.to_csv("data/user_item_lookup.csv")
+    multi_df.to_csv(file, sep="\t")
 
     # Return the User/Item matrix subset of the dataframe
     return (merge_df.loc[:, "ItemIndex":"UserIndex"])
+
+
+def load_or_fetch_ui(df, file):
+    if os.path.isfile(file):
+        print("Loading pre-existing User/Item Matrix...")
+        df = pd.read_csv(file, sep="\t")
+        return (df.loc[:, "UserIndex":"ItemIndex"])
+    else:
+        print("File not found. Creating User/Item Matrix")
+        return user_item_sparse(df, file)
 
 
 if __name__ == "__main__":
@@ -59,7 +73,13 @@ if __name__ == "__main__":
 
     # Load the traing and test data as multiindex dataframes
     train_df = init_dataframe(training_file, f_training_file)
-    test_df = init_dataframe(test_file, f_test_file)
+    # test_df = init_dataframe(test_file, f_test_file)
 
     # Get a User/Item matrix from the training data
-    user_item_matrix_df = user_item_sparse(train_df)
+    user_item_df = load_or_fetch_ui(train_df, "data/user_item_lookup.csv")
+    user_item_matrix = user_item_df.as_matrix()
+
+    cos_norm = pp.normalize(user_item_matrix, axis=0)
+    user_sim = cos_norm.dot(cos_norm.T)
+    user_sim_df = pd.DataFrame(user_sim)
+    print(user_sim)
